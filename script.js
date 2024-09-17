7a0852d543e4cca9b280c7d828219c269e9e639a
FuadTalukder85 <webfuadtk@gmail.com>
2024-09-18 00:57:23 +0600

    complete part 01

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-gsap.to("#box1", {
-  x: 1500,
-  rotate: 360,
-  duration: 1.5,
+// gsap.to("#box1", {
+//   x: 1500,
+//   rotate: 360,
+//   duration: 1.5,
+//   delay: 1,
+// });
+// gsap.to("#box2", {
+//   x: 1500,
+//   backgroundColor: "yellow",
+//   duration: 1.5,
+//   delay: 2.5,
+// });
+// gsap.to("#box3", {
+//   x: 1500,
+//   borderRadius: "50%",
+//   duration: 1.5,
+//   delay: 3.5,
+// });
+
+var tl = gsap.timeline();
+
+tl.from("h2", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+});
+tl.from("h4", {
+  y: -20,
+  opacity: 0,
+  duration: 1,
+  delay: 0.5,
+  stagger: 0.3,
+});
+tl.from("h1", {
+  y: 20,
+  opacity: 0,
+  duration: 1,
+  scale: 0.2,
 });
